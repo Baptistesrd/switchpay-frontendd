b45e608440caf3a9207bb2966a3077b0cf92da49
Baptistesrd <baptiste.sardou@edhec.com>
2026-04-18 00:12:27 +0100

    navbar fx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,17 @@
-import { Box } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
-import { useColorMode } from "@chakra-ui/react";
-
-const MotionBox = motion(Box);
 
 export default function Layout({ children }) {
-  const { colorMode } = useColorMode();
-
   return (
     <AnimatePresence mode="wait">
-      <MotionBox
-        key={colorMode}
+      <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.35 }}
-        minH="100vh"
+        style={{ minHeight: "100vh" }}
       >
         {children}
-      </MotionBox>
+      </motion.div>
     </AnimatePresence>
   );
 }
